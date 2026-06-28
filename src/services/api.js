9af77982704999eb7f122bb2fc6d592e994d6098
Ninzkie1 <ningarbo15@gmail.com>
2026-06-28 13:45:45 +0800

    backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getAccessToken, getStoredUser, updateStoredUser, clearStoredUser } from '../utils/authStorage';
 
-const API_URL = 'http://localhost:5001/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001/api';
 
 const api = axios.create({
   baseURL: API_URL,
